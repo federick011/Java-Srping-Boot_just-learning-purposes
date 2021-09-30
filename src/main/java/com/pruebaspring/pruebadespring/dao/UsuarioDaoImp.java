ceaf3927ba5005fce3e3c3e986624daa1f613a50
FEDERICK TURIZO <federick.turizo@gmail.com>
2021-09-29 21:05:54 -0500

    Implement Argon2, hash

--- src/main/java/com/pruebaspring/pruebadespring/dao/UsuarioDaoImp.java
+++ src/main/java/com/pruebaspring/pruebadespring/dao/UsuarioDaoImp.java
@@ -1,6 +1,8 @@
 package com.pruebaspring.pruebadespring.dao;
 
 import com.pruebaspring.pruebadespring.models.Usuario;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -31,6 +33,8 @@ public class UsuarioDaoImp implements  UsuarioDao
     @Override
     public List<Usuario> GetUser(long id) {//parametro del WHERE
         String query = "FROM Usuario WHERE id = :id";
+
+
         return entityManager.createQuery(query)
                 .setParameter("id", id)
                 .getResultList();
@@ -46,7 +50,18 @@ public class UsuarioDaoImp implements  UsuarioDao
     @Override
     public void Registrar(Usuario usuario)
     {
+
+        //Save password as value and then save into DB
+        usuario.setPassword(hashPassword(usuario));
         entityManager.merge(usuario);
+    }
+    
+    private  String hashPassword(Usuario usuario)
+    {
+        //Hashing password
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2d);
+        String hash = argon2.hash(3, 1024, 1, usuario.getPassword());
+        return hash;
     }
 
     @Override
